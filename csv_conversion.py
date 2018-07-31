a5f05c732f01ab49e20b776d6020df71b97fc08a
Adam Hawley <adamhawley99@gmail.com>
2018-08-01 00:48:24 +0100

    Update csv_conversion.py

--- csv_conversion.py
+++ csv_conversion.py
@@ -1,6 +1,18 @@
 import csv
 
 def parse_csv(filename):
+        f = open(filename, 'U')
+
+        newText = f.read()
+
+        while ": \n" in newText:
+                newText = newText.replace(": \n", ": |")
+        f.close()
+
+        with open(filename, 'w') as f:
+                f.write(newText)
+        f.close()
+
         file = open(filename, 'r')
         elements = csv.reader(file)
         arr = []
@@ -37,30 +49,47 @@ def find_att(row, attr_number):
                         return att
 
 def create_contacts():
-        company_names = ["Shefflets", "Northwood Northampton","Sewell & Gardner", "Location Location"]
+        company_names = ["Shefflets", "Northwood Northampton","Sewell & Gardner", "Location Location", "Saxon Kings"]
         return company_names
 
-def write_csv():
-        file = open("output.csv", "wb")
+
+def create_header(writer):
+        row = ["Title","First Name","Last Name","Mobile Number","Email Address","Address Line 1","Address Line 2","Adress Line 3","City","Postcode","Move In Date","Branch ID","Branch Name","Agent Name","Agent Email"]
+        writer.writerow(row)
+        return
+
+def write_csv(contact_list):
+        file = open("output.csv", "w")
         w = csv.writer(file, dialect='excel')
-        s.
+        create_header(w)
+        fill_csv(w, contact_list)
+        return
+
+def fill_csv(w, contact_list):
+        for contact in contact_list:
+            row = ["Unknown", contact.fname, contact.lname, contact.phone, contact.email, contact.add_one, contact.add_two, contact.add_three, contact.city, contact.postcode, contact.move_in, "Unknown", contact.name, "Unknown", "Unknown"]
+            w.writerow(row)
+        return
 
 def main():
         arr = parse_csv("test.csv")
         companies = {}
         ies_in_csv = []
+        other_in_csv = []
         for i in range(len(arr)):
                 name = find_att(arr[i][0], 0)
-                if name != None:
-                        companies[name] = arr[i][0]
+                if name != "":
+                        try:
+                                companies[name].append(arr[i][0])
+                        except KeyError:
+                            companies[name] = arr[i]
+
         ies_comps = create_contacts()
         for i in companies.keys():
                 if i in ies_comps:
-                        ies_in_csv.append(contact(companies[i]))
-        test_name = "Sewell & Gardner"
-        ex = contact(companies[test_name])
-        print(ex.__dict__)
-        print("___________________")
+                        for j in companies[i]:
+                                ies_in_csv.append(contact(j))
         print(ies_in_csv)
+        write_csv(ies_in_csv)
 
 main()
